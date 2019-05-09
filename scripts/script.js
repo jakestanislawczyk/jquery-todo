670c7eda1dcbb792199001739359da58b2c75b71
Jake <jakestanislawczyk@gmail.com>
2019-05-09 14:20:18 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var tasknum = 1;
+    $("#add").click(function() {
+        $("ol").append("<ul>Task no. " + tasknum + "</ul>");
+        tasknum++;
+    });
+
+    $("#delete").click(function() {
+        if (tasknum > 1) {
+            $("ul")
+                .last()
+                .detach();
+            tasknum--;
+        }
+    });
+
+    $("#clear").click(function() {
+        $("ol").empty();
+        tasknum = 1;
+    });
+
+    $("ol").sortable();
+});
